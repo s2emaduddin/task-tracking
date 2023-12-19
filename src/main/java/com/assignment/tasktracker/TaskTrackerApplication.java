4c205b425643bc128a5220a8ea87a1258bfb9995
s2emaduddin <emaduddin.ms@gmail.com>
2023-12-19 00:08:00 -0500

    Bug fix for swagger

--- src/main/java/com/assignment/tasktracker/TaskTrackerApplication.java
+++ src/main/java/com/assignment/tasktracker/TaskTrackerApplication.java
@@ -2,10 +2,8 @@ package com.assignment.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
